3dec6ca39e81abde4b8b00ce12c677535cb8c6a1
Greice <greicefcardoso@gmail.com>
2020-11-27 09:54:46 -0300

    Created routes

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
 
-router.get('/categories', (req,res)=>{
-    res.send("Rota de categorias");
+
+router.get('/admin/categories/new', (req,res)=>{
+    res.render('admin/categories/new');
+})
+
+router.post('/categories/save', (req,res) =>{
+    let title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title:title,
+            slug:slugify(title)
+        }).then(() => {
+            res.redirect('/');
+        })
+    } else {
+        res.redirect("/admin/categories/new");
+    }
+})
+
+router.get('/admin/categories', (req,res)=>{
+    Category.findAll().then(categories=>{
+        res.render('admin/categories/index',{
+            categories:categories
+        });
+    });
 })
 
 module.exports = router;
