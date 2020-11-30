cb614c1b4e1a2d4022b4b6ad391b50fbfc6c1de6
Greice <greicefcardoso@gmail.com>
2020-11-30 18:43:56 -0300

    Edit and delete routes

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -15,7 +15,7 @@ router.post('/categories/save', (req,res) =>{
             title:title,
             slug:slugify(title)
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         })
     } else {
         res.redirect("/admin/categories/new");
@@ -28,6 +28,55 @@ router.get('/admin/categories', (req,res)=>{
             categories:categories
         });
     });
+})
+
+router.post('/categories/delete',(req,res)=>{
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id:id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        }else{
+            //not a number
+            res.redirect("/admin/categories");
+        }
+    } else{
+        //null
+        res.redirect("/admin/categories");
+    }
+})
+
+router.get('/admin/categories/edit/:id', (req,res) =>{
+    let id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories"); 
+    }
+
+    Category.findByPk(id).then(category =>{
+        if(category != undefined){
+            res.render("admin/categories/edit",{category:category});
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(error =>{
+        res.redirect("/admin/categories");
+    })
+})
+
+router.post('/categories/update', (req,res) =>{
+    let id = req.body.id;
+    let title = req.body.title;
+    Category.update({title:title, slug:slugify(title)},
+        {
+            where: {id:id}
+        }).then(() =>{
+            res.redirect("/admin/categories");
+        })
 })
 
 module.exports = router;
