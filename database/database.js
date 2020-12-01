d9d1b410cc51397e494ce2c6b9e65c87794bde7d
Greice <greicefcardoso@gmail.com>
2020-12-01 18:41:19 -0300

    Added timezone

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('Blog-painel-adm','root','12345',{
     host:'localhost',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone:"-03:00"
 })
 
 module.exports = connection;
