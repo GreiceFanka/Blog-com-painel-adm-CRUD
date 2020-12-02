12e450ba7909e83effab3522b1e1d27ea7d08cd4
Greice <greicefcardoso@gmail.com>
2020-12-02 19:31:32 -0300

    Added routes

--- index.js
+++ index.js
@@ -28,7 +28,30 @@ app.use('/',categoriesController);
 app.use('/',articlesController);
 
 app.get('/', (req,res) =>{
-    res.render("index");
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles =>{
+        res.render("index",{articles:articles});
+    })
+})
+
+app.get('/:slug',(req,res) =>{
+    let slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug:slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            res.render("article",{article:article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(error =>{
+        res.redirect("/");
+    })
 })
 
 app.listen(3002,()=>{
