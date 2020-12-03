345b12ab8ace9e0a6d4c8c861727b42be0d294d6
Greice <greicefcardoso@gmail.com>
2020-12-03 18:54:10 -0300

    Created Route

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.get('/', (req,res) =>{
             ['id','DESC']
         ]
     }).then(articles =>{
-        res.render("index",{articles:articles});
+        Category.findAll().then(categories =>{
+            res.render("index",{articles:articles, categories:categories});
+        })    
     })
 })
 
@@ -45,12 +47,34 @@ app.get('/:slug',(req,res) =>{
         }
     }).then(article =>{
         if(article != undefined){
-            res.render("article",{article:article});
+            Category.findAll().then(categories =>{
+                res.render("article",{article:article, categories:categories});
+            })
         }else{
             res.redirect("/");
         }
     }).catch(error =>{
         res.redirect("/");
+    })
+})
+
+app.get('/category/:slug', (req,res) =>{
+    let slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model:Article}]
+    }).then(category =>{
+        if(category != undefined){
+            Category.findAll().then(categories =>{
+                res.render("index",{articles: category.articles, categories:categories});
+            })
+        }else{
+            res.redirect('/');
+        }
+    }).catch(error =>{
+        res.redirect('/');
     })
 })
 
