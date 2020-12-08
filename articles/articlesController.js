e26443085b1b5009bdcd75102eb403d3f2354269
Greice <greicefcardoso@gmail.com>
2020-12-08 19:29:49 -0300

    Added routes

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -55,4 +55,72 @@ router.post('/articles/delete',(req,res)=>{
         res.redirect("/admin/articles");
     }
 })
+
+router.get('/admin/articles/edit/:id', (req,res) =>{
+        let id = req.params.id;
+        Article.findByPk(id).then(article => {
+            if(article != undefined){
+                Category.findAll().then(categories =>{
+                    res.render("admin/articles/edit",{categories:categories, article:article});
+                })
+            } else{
+                res.redirect("/");
+            }
+        }).catch(error =>{
+            res.redirect("/");
+    })
+})
+
+router.post('/articles/update', (req,res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+    Article.update({
+        title:title, 
+        body:body,
+        categoryId:category,
+        slug:slugify(title)
+
+    },{
+        where:{
+            id:id
+        }   
+    }).then(() =>{
+        res.redirect("/admin/articles");
+    }).catch(error =>{
+        res.redirect("/");
+    })
+
+})
+
+router.get('/articles/page/:num', (req,res) => {
+    let page = req.params.num;
+    let offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0;
+    } else {
+        offset = parseInt(page) * 4;
+    }
+
+    Article.findAndCountAll({
+      limit:4,
+      offset: offset
+    }).then(articles =>{
+        let next;
+        if(offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+        let result = {
+            next:next,
+            articles:articles
+        }
+
+        res.json(result);
+    })
+})
+
 module.exports = router;
