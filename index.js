b065de91ab703128c7e3deb1e6f5cf8aa65ba1c9
Greice <greicefcardoso@gmail.com>
2020-12-10 20:06:55 -0300

    Made some changes to routes

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ app.get('/', (req,res) =>{
     Article.findAll({
         order:[
             ['id','DESC']
-        ]
+        ],
+        limit:4
     }).then(articles =>{
         Category.findAll().then(categories =>{
             res.render("index",{articles:articles, categories:categories});
